c019915bc9cafd80d4b0311ce9baa10f89534e43
Nico <nicodiazan@gmail.com>
2023-09-14 11:54:32 +0200

    Add tests for reverseString

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,19 @@
 import functions from "./functions";
 
+//Should capitalize the first letter
 it("Capitalize 1", () => {
 	expect(functions.capitalize("asdf")).toBe("Asdf");
 });
 
 it("Capitalize 2", () => {
 	expect(functions.capitalize("ASDF")).toBe("ASDF");
+});
+
+//Takes a string and returns it reversed
+it("Reverse String 1", () => {
+	expect(functions.reverseString("asdf")).toBe("Asdf");
+});
+
+it("Reverse String 2", () => {
+	expect(functions.reverseString("ASDF")).toBe("ASDF");
 });
